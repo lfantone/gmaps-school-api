204a8aae0e04a59c23737d36cca3897cd3fc9a5c
Nicolás Fantone <nfantone@gmail.com>
2016-04-03 03:35:41 -0300

    Create basic Koa app

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Koa v2 API for providing geolocalization data
+ * of Buenos Aires schools.
+ */
+const path = require('path');
+require('app-module-path').addPath(path.join(__dirname, 'app'));
+const log = require('logger');
+const server = require('server');
+const config = require('config');
+
+// Starts Koa server
+server.listen(config.get('koa:port'), config.get('koa:hostname'), () => {
+  log.info('✔ Koa server listening on %s:%s [%s]', config.get('koa:hostname'),
+    config.get('koa:port'), config.get('env'));
+});
+
+module.exports = server;
