dcf2dc34653880c0fd2ec9c80129f4ea746e13b0
Nicolás Fantone <nfantone@gmail.com>
2016-04-03 03:36:39 -0300

    Add endpoint to fetch schools from a JSON file

---
+++ app/routes/schools.js
@@ -0,0 +1,26 @@
+'use strict';
+/**
+ * Implementation of API /schools endpoint.
+ * @module schools
+ */
+const schools = require('config/schools');
+
+/**
+ * Set up /schools endpoint.
+ * @param  {Object} router A Koa router
+ */
+module.exports = function(router) {
+  /**
+   * GET /schools
+   *
+   * Returns information about schools located
+   * in Buenos Aires and their geolocation data.
+   */
+  router.get('/schools', (ctx) => {
+    ctx.status = 200;
+    ctx.body = {
+      success: true,
+      data: schools
+    };
+  });
+};
